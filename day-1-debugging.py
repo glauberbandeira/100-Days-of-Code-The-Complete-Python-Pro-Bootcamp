eb758dcb76c26d576e1804933fef9bcb43a895ac
glauberbandeira <glauberbandeira_@hotmail.com>
2024-05-30 17:31:22 +1000

    correcting lesson 2

--- day-1-debugging.py
+++ day-1-debugging.py
@@ -12,6 +12,6 @@ print(("New lines can be created with a backslash and n.")
 '''
 
 print("Day 1 - String Manipulation")
-print("String Concatenation is done with the "+" sign.")
-print("e.g. print('Hello " + "world')")
+print('String Concatenation is done with the "+" sign.')
+print('e.g. print("Hello " + "world")')
 print("New lines can be created with a backslash and n.")
